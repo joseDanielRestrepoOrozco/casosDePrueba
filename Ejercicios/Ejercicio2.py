112878f2b6483ca62d603fb70a77de05404f96cc
dani-agudelo <daniiagudelom@gmail.com>
2024-12-02 09:11:55 -0500

    Ejercicio 2 listo

---
+++ Ejercicios/Ejercicio2.py
@@ -0,0 +1,7 @@
+'''
+Un sistema de tránsito registra vehículos con las siguientes reglas:
+
+Placa: debe ser un texto de 6 o 7 caracteres.
+Año de fabricación: debe ser entre 1980 y el año actual.
+Tipo de vehículo: debe ser "automóvil", "camión", "moto" o "bus".
+'''
