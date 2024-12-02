c93fff2c2d3e1b4ae9d4c977dcb37dc4038be136
JoseDanielRestrepoOrozco <jose.resoro4789@gmail.com>
2024-12-02 09:31:10 -0500

    add exercise 1

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 import unittest
+
+cadena1 = "Python123"
+cadena2 = "Python 123"
+cadena3 = "12345"
+cadena4 = ""
+
+print(cadena1.isalnum())  # True: solo contiene letras y números
+print(cadena2.isalnum())  # False: contiene un espacio
+print(cadena3.isalnum())  # True: solo contiene números
+print(cadena4.isalnum())  # False: está vacía
